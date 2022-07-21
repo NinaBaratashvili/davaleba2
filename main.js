a86ae2789b814e520bbe40ebf2dffbd3a1e1b2a9
NinaBaratashvili <105943239+NinaBaratashvili@users.noreply.github.com>
2022-07-21 11:38:11 +0200

    update

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
 document.getElementById("texsti"); innerHTML = "BREEF PEAKS BEYOND"
 
-document.write("Hello!");
+
 
 console.log("hello console log")
 
 window.alert("Pleace, accept the coockies");
+const cookie = "user=hussein; samesite=lax; secure";
 
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
+/* (ask Salome)
+menuToggle.addEventListener('click', () => {menuToggle.classList.toggle('active'); showcase.classList.toggle('active');
 })
+*/
 
+/* 
+const nav = ["Home", "News", "Authors", "Interviews", "Contact" ];
+
+let nav = nav.length;
+let navtxt = "<ul>";
+
+for(let i=0; i<nav1; i++) {
+   navtxt += '<li><a href="">' + nav[i] + "</a></li>";
+}
+
+navtxt += "</ul>";
+
+document.getElementById("nav").innerHTML = navtxt;
+*/
+
+const nav = ["Home", "News", "Authors", "Interviews", "Contact"];
+const links = ["index.html", "news.html", "authors.html", "interviews.html", "contact.html"];
+
+let navl = nav.length;
+let navtxt = "<ul>";
+
+for(let i=0; i<navl; i++){
+    navtxt += '<li><a href=" ' + links[i] + ' ">' + nav[i] + "</a></li>";
+}
+
+navtxt += "</ul>";
+
+document.getElementById("nav").innerHTML = navtxt;
+/*
+function gotoauthors() {
+  let explore = document.getElementById("social")
+} 
+*/
+
+console.log("screenwidth: " + screen.width);
+console.log("screenheight: " + screen.height);
+
+console.log("possiblewidth: " + screen.availWidth);
+console.log("possibleheight: " + screen.availHeight);
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
